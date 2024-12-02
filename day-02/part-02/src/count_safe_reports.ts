afe7420966b0c162abf0ab755e0f7d80c2202fd5
Emre AKA <aka548548@gmail.com>
2024-12-02 22:56:27 +0300

    refactor(day-02/part-02): extract report safety check into separate function and update tests

--- day-02/part-02/src/count_safe_reports.ts
+++ day-02/part-02/src/count_safe_reports.ts
@@ -1,47 +1,46 @@
+function checkIfReportIsSafe(report: number[]) {
+  let isSafe = true;
+
+  // decrease
+  for (let index = 1; index < report.length; index++) {
+    const curr = report[index];
+    const prev = report[index - 1];
+    const diff = Math.abs(prev - curr);
+
+    //unsafe
+    if (prev === curr || prev < curr || diff < 1 || diff > 3) isSafe = false;
+  }
+
+  // if not set false already, it means it is safe...
+  // it will increase the count and continue to the next report
+  // else it will check by increase
+  if (isSafe) return true
+
+  // because is set false in the above loop,
+  // we reset the state
+  isSafe = true;
+
+  // increase
+  for (let index = 1; index < report.length; index++) {
+    const curr = report[index];
+    const prev = report[index - 1];
+    const diff = Math.abs(prev - curr);
+
+    //unsafe
+    if (prev === curr || prev > curr || diff < 1 || diff > 3) isSafe = false;
+  }
+
+  // if not set false , it means it is safe...
+  // it will increase the count and continue to the next report
+  if (isSafe) return true
+}
+
 export function countSafeReports(reports: number[][]) {
   let result = 0;
 
   for (const report of reports) {
-    let isSafe = true;
-
-    // decrease
-    for (let index = 1; index < report.length; index++) {
-      const curr = report[index];
-      const prev = report[index - 1];
-      const diff = Math.abs(prev - curr)
-
-      //unsafe
-      if (prev === curr || prev < curr || diff < 1 || diff > 3) isSafe = false;
-    }
-
-    // if not set false already, it means it is safe...
-    // it will increase the count and continue to the next report
-    // else it will check by increase
-    if (isSafe) {
-      result++;
-      continue;
-    }
-
-    // because is set false in the above loop,
-    // we reset the state 
-    isSafe = true
-
-    // increase
-    for (let index = 1; index < report.length; index++) {
-      const curr = report[index];
-      const prev = report[index - 1];
-      const diff = Math.abs(prev - curr)
-
-      //unsafe
-      if (prev === curr || prev > curr || diff < 1 || diff > 3) isSafe = false;
-    }
-
-    // if not set false , it means it is safe...
-    // it will increase the count and continue to the next report
-    if (isSafe) {
-      result++;
-      continue;
-    }
+    const isSafe = checkIfReportIsSafe(report)
+    if (isSafe) result++
   }
 
   return result;
--- day-02/part-02/src/main_test.ts
+++ day-02/part-02/src/main_test.ts
@@ -17,24 +17,30 @@ Deno.test(async function shouldParseData() {
   ]);
 });
 
-Deno.test(function shouldCountSafeReports() {
-  /* const data = [
-    [1, 2, 3, 4, 5],
-    [1, 2, 1, 4, 5],
-  ]; */
-
-  /* const data = [
-    [7, 6, 4, 2, 1],
-    [1, 2, 7, 8, 9],
-    [9, 7, 6, 2, 1],
-    [1, 3, 2, 4, 5],
-    [8, 6, 4, 4, 1],
-    [1, 3, 6, 7, 9],
-  ]; */
-
+Deno.test(function shouldCountSafeReportsRealData() {
   const data = parseData(Deno.readTextFileSync("public/data.txt"));
 
   const safeReportCount = countSafeReports(data);
 
   assertEquals(safeReportCount, 402);
+});
+
+
+Deno.test(function shouldCountSafeReportsExampleData() {
+  const data = parseData(Deno.readTextFileSync("public/example-data.txt"));
+
+  const safeReportCount = countSafeReports(data);
+
+  assertEquals(safeReportCount, 2);
+});
+
+Deno.test(function shouldCountSafeReportsEdgeCase() {
+  const data = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 1, 4, 5],
+  ];
+
+  const safeReportCount = countSafeReports(data);
+
+  assertEquals(safeReportCount, 2);
 });
