f670e2814f094c03423bfcf14bf74783cc67119f
Emre AKA <aka548548@gmail.com>
2024-12-02 17:19:27 +0300

    refactor(day-2): simplify trend checking logic in countSafeReports function - part-01

--- day-02/part-01/count_safe_resports.ts
+++ day-02/part-01/count_safe_resports.ts
@@ -1,71 +1,44 @@
-function getTrendStats(report: number[]) {
-	let trendStatus: "increasing" | "decreasing" | "stable" = "stable";
+function checkIfReportFollowsTrend(report: number[]): boolean {
+  const [first, second] = report;
 
-	if (report[0] > report[1]) trendStatus = "decreasing";
-	else if (report[0] < report[1]) trendStatus = "increasing";
-	else trendStatus = "stable";
+  if (first > second) {
+    for (let index = 0; index < report.length; index++) {
+      const prev = report[index - 1];
+      const curr = report[index];
 
-	return {
-		trendStatus,
-		report,
-	};
+      if (index > 0 && (prev === curr || prev < curr)) return false;
+    }
+  }
+  if (first < second) {
+    for (let index = 0; index < report.length; index++) {
+      const prev = report[index - 1];
+      const curr = report[index];
+
+      if (index > 0 && (prev === curr || prev > curr)) return false;
+    }
+  }
+
+  return true;
 }
 
-function checkIfReportFollowsTrend({
-	trendStatus,
-	report,
-}: {
-	trendStatus: "increasing" | "decreasing" | "stable";
-	report: number[];
-}): boolean {
-	const result = report.map((report, index, reports) => {
-		const prev = reports[index - 1];
-		if (
-			trendStatus === "increasing" &&
-			index > 0 &&
-			(prev === report || prev > report)
-		) {
-			return false;
-		}
-		if (
-			trendStatus === "decreasing" &&
-			index > 0 &&
-			(prev === report || prev < report)
-		) {
-			return false;
-		}
+function checkIfLevelsWithinRange(report: number[]): boolean {
+  for (let index = 1; index < report.length; index++) {
+    const prev = report[index - 1];
+    const curr = report[index];
 
-		return true;
-	});
+    const diff = Math.abs(prev - curr);
 
-	return result.every((item) => item);
-}
+    if (diff < 1 || diff > 3) return false;
+  }
 
-function checkIfLevelsWithinRange({
-	report,
-}: {
-	trendStatus: "increasing" | "decreasing" | "stable";
-	report: number[];
-}): boolean {
-	for (let index = 1; index < report.length; index++) {
-		const prev = report[index - 1];
-		const curr = report[index];
-
-		const diff = Math.abs(prev - curr);
-
-		if (diff < 1 || diff > 3) return false;
-	}
-
-	return true;
+  return true;
 }
 
 export function countSafeReports(reports: number[][]) {
-	const result = reports
-		.map(getTrendStats)
-		.filter((report) => report.trendStatus !== "stable")
-		.filter(checkIfReportFollowsTrend)
-		.filter(checkIfLevelsWithinRange)
-		.reduce((prev) => prev + 1, 0);
+  const result = reports
+    .filter(checkIfReportFollowsTrend)
+    .filter(checkIfLevelsWithinRange)
+    .reduce((prev) => prev + 1, 0);
 
-	return result;
+  return result;
 }
