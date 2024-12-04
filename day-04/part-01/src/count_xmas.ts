085c0c94bb2c181edf3ca06da2f01cc885abed4c
Emre AKA <aka548548@gmail.com>
2024-12-04 23:26:10 +0300

    feat(day-04/part-01): enhance countXmas function to support diagonal counting and improve readability

--- day-04/part-01/src/count_xmas.ts
+++ day-04/part-01/src/count_xmas.ts
@@ -1,46 +1,128 @@
 export function countXmas(value: string[][]): number {
-	let result = 0;
-    const matixLength = value[0].length;
+  let result = 0;
 
-	for (let i = 0; i < matixLength; i++) {
-        for(let j = 0; j < matixLength; j++) {
-            const char = value[i][j]
+  for (let i = 0; i < value.length; i++) {
+    for (let j = 0; j < value[i].length; j++) {
+      const char = value[i][j];
 
-            if (char !== 'X') continue
+      if (char !== "X") continue;
 
-            result++
+      const operations = [
+        getHorizantalLeft(j, value[i]),
+        getHorizantalRight(j, value[i]),
+        getVerticalUp({ row: i, col: j }, value),
+        getVerticalDown({ row: i, col: j }, value),
+        getDiagonalUpLeft({ row: i, col: j }, value),
+        getDiagonalUpRight({ row: i, col: j }, value),
+        getDiagonalBottomLeft({ row: i, col: j }, value),
+        getDiagonalBottomRight({ row: i, col: j }, value),
+      ];
 
-            const left = getHorizantalLeft(j, value[i])
-            const right = getHorizantalRight(j, value[i])
-            const up = getVerticalUp({row: i, col: j}, value)
-            const down = getVerticalDown({row: i, col: j}, value)
-            console.log("left:", left, "right:", right, "up:", up, "down:", down)
-        }
+      operations.forEach((span) => {
+        if (span === "XMAS") result++;
+      });
     }
+  }
 
-	return result;
+  return result;
 }
 
 function getHorizantalLeft(startIndex: number, chars: string[]) {
-    const span = chars.slice(startIndex - 3,startIndex + 1).reverse()
-    return span.toString().replaceAll(',', '')
+  const span = chars.slice(startIndex - 3, startIndex + 1).reverse();
+  return span.toString().replaceAll(",", "");
 }
 
 function getHorizantalRight(startIndex: number, chars: string[]) {
-    const span = chars.slice(startIndex, startIndex + 4)
-    return span.toString().replaceAll(',', '')
+  const span = chars.slice(startIndex, startIndex + 4);
+  return span.toString().replaceAll(",", "");
 }
 
-function getVerticalUp({row, col}: {row: number, col: number}, matrix: string[][]) {
-    if (row < 3) return ''
+function getVerticalUp(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row < 3) return "";
 
-    const span = [matrix[row][col], matrix[row - 1][col], matrix[row - 2][col], matrix[row - 3][col]]
-    return span.toString().replaceAll(',', '')
+  const span = [
+    matrix[row][col],
+    matrix[row - 1][col],
+    matrix[row - 2][col],
+    matrix[row - 3][col],
+  ];
+  return span.toString().replaceAll(",", "");
 }
 
-function getVerticalDown({row, col}: {row: number, col: number}, matrix: string[][]) {
-    if (row < matrix.length - 3) return ''
+function getVerticalDown(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row > matrix.length - 4) return "";
 
-    const span = [matrix[row][col], matrix[row - 1][col], matrix[row - 2][col], matrix[row - 3][col]]
-    return span.toString().replaceAll(',', '')
+  const span = [
+    matrix[row][col],
+    matrix[row + 1][col],
+    matrix[row + 2][col],
+    matrix[row + 3][col],
+  ];
+  return span.toString().replaceAll(",", "");
+}
+
+function getDiagonalUpLeft(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row < 3 || col < 3) return "";
+
+  const span = [
+    matrix[row][col],
+    matrix[row - 1][col - 1],
+    matrix[row - 2][col - 2],
+    matrix[row - 3][col - 3],
+  ];
+  return span.toString().replaceAll(",", "");
+}
+
+function getDiagonalUpRight(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row < 3 || col > matrix.length - 4) return "";
+
+  const span = [
+    matrix[row][col],
+    matrix[row - 1][col + 1],
+    matrix[row - 2][col + 2],
+    matrix[row - 3][col + 3],
+  ];
+  return span.toString().replaceAll(",", "");
+}
+
+function getDiagonalBottomLeft(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row > matrix.length - 4 || col < 3) return "";
+
+  const span = [
+    matrix[row][col],
+    matrix[row + 1][col - 1],
+    matrix[row + 2][col - 2],
+    matrix[row + 3][col - 3],
+  ];
+  return span.toString().replaceAll(",", "");
+}
+
+function getDiagonalBottomRight(
+  { row, col }: { row: number; col: number },
+  matrix: string[][]
+) {
+  if (row > matrix.length - 4 || col > matrix.length - 4) return "";
+
+  const span = [
+    matrix[row][col],
+    matrix[row + 1][col + 1],
+    matrix[row + 2][col + 2],
+    matrix[row + 3][col + 3],
+  ];
+  return span.toString().replaceAll(",", "");
 }
--- day-04/part-01/src/main_test.ts
+++ day-04/part-01/src/main_test.ts
@@ -3,30 +3,40 @@ import { parseData } from "./parser.ts";
 import { countXmas } from "./count_xmas.ts";
 
 Deno.test(async function shouldParseData() {
-    const data = await Deno.readTextFile("public/example-data.txt")
+  const data = await Deno.readTextFile("public/example-data.txt");
 
-    const result = parseData(data);
+  const result = parseData(data);
 
-    assertEquals(result, [
-        ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"],
-        ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"],
-        ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"],
-        ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"],
-        ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"],
-        ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"],
-        ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"],
-        ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"],
-        ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"],
-        ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"]
-      ])
+  assertEquals(result, [
+    ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"], // 0
+    ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"], // 1
+    ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"], // 2
+    ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"], // 3
+    ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"], // 4
+    ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"], // 5
+    ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"], // 6
+    ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"], // 7
+    ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"], // 8
+    ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"], // 9
+  ]);
 });
 
 Deno.test(async function shouldCountXmas() {
-    const data = await Deno.readTextFile("public/example-data.txt")
+  const data = await Deno.readTextFile("public/example-data.txt");
 
-    const result = parseData(data);
+  const result = parseData(data);
 
-    const count = countXmas(result)
+  const count = countXmas(result);
 
-    assertEquals(count, 18)
+  assertEquals(count, 18);
+});
+
+Deno.test(function shouldCountTwo() {
+  const result = [
+    ["S","A","M","X","M","A","S"],
+  ];
+
+  const count = countXmas(result);
+
+  assertEquals(count, 2);
 });
